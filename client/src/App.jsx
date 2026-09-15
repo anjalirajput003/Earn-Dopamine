f5792eb90cf97bf2d718c5b49be9dce5561f21ac
anjalirajput003 <anjalirajput0403@gmail.com>
2026-09-15 21:29:24 +0530

    frontend initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <h1 className="p-8 text-3xl font-bold">Earn Dopamine</h1>
+    </div>
+  );
+}
+
+export default App;
