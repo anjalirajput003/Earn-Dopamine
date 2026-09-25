8a3dbd9f1ec64496bde7f6d6aa64286efac8211b
anjalirajput003 <anjalirajput0403@gmail.com>
2026-09-25 16:22:55 +0530

    polling issue fixed

--- client/src/services/socket/socket.js
+++ client/src/services/socket/socket.js
@@ -6,6 +6,7 @@ const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
   withCredentials: true,
+  transports: ["websocket"],
 });
 
 export const connectSocket = () => {
